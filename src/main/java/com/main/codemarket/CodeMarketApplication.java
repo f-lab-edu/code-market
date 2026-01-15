e2e262c65c155e12d43dc36e1f38c453acf5b4d8
KimYoonMin <dbsalszz@naver.com>
2026-01-15 22:10:34 +0900

    chore(main): 초기 프로젝트 main 함수, main 테스트 함수 추가

---
+++ src/main/java/com/main/codemarket/CodeMarketApplication.java
@@ -0,0 +1,13 @@
+package com.main.codemarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeMarketApplication.class, args);
+    }
+
+}
