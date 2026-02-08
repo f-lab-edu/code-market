7e76490d741e370c4c82c134260a8234eae1f6a6
KimYoonMin <dbsalszz@naver.com>
2026-02-08 22:26:32 +0900

    feat(auth): 회원 인증 기능 신규 개발 - 인증용 커스텀 회원 객체 추가

---
+++ src/main/java/com/main/codemarket/security/CustomUserDetail.java
@@ -0,0 +1,32 @@
+package com.main.codemarket.security;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+public class CustomUserDetail implements UserDetails {
+    private String email;
+    private String username;
+    private String password;
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
