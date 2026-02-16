ae475a3bcb4002ee0a7dff51d8ebf3d4acf8dffa
KimYoonMin <dbsalszz@naver.com>
2026-02-16 20:32:37 +0900

    feat(auth): 로그인필터 단위테스트 작성 - 로그인 인증 기능 단위 테스트 작성

--- src/test/java/com/main/codemarket/security/filter/LoginFilterTest.java
+++ src/test/java/com/main/codemarket/security/filter/LoginFilterTest.java
@@ -1,20 +1,96 @@
 package com.main.codemarket.security.filter;
 
-import org.junit.jupiter.api.Test;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.main.codemarket.security.JwtUtil;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.InternalAuthenticationServiceException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
 class LoginFilterTest {
+    @Mock
+    private AuthenticationManager authenticationManager;
+    @Mock
+    private JwtUtil jwtUtil;
 
-    @Test
-    void attemptAuthentication() {
+    private ObjectMapper objectMapper;
+
+
+    private LoginFilter loginFilter;
+
+    private MockHttpServletRequest request;
+
+    private MockHttpServletResponse response;
+
+    @BeforeEach
+    void setUp() {
+        objectMapper = new ObjectMapper();
+        loginFilter = new LoginFilter(authenticationManager, jwtUtil, objectMapper);
+        request = new MockHttpServletRequest();
+        response = new MockHttpServletResponse();
+
+        request.setMethod("POST");
+        request.setContentType("application/json");
     }
 
     @Test
-    void successfulAuthentication() {
+    @DisplayName("인증에 필요한 응답 값을 파싱하고, 인증을 진행한다")
+    void attemptAuthentication_success() throws IOException {
+        //given
+        String jsonBody = "{\"email\":\"test@test.com\",\"password\":\"123456789\"}";
+        request.setContent(jsonBody.getBytes(StandardCharsets.UTF_8));
+
+        //when
+        loginFilter.attemptAuthentication(request, response);
+
+        //실제 인증 과정 호출하였는지 테스트
+        verify(authenticationManager).authenticate(any(UsernamePasswordAuthenticationToken.class));
     }
 
     @Test
-    void unsuccessfulAuthentication() {
+    @DisplayName("로그인 인증에 필요한 값이 파싱이 되지 않는다면 예외를 던진다")
+    void loginRequest_format_invalid() {
+        //given
+        String jsonBody = "{\"mail\":\"test@test.com\",\"password\":\"123456789\"}";
+        request.setContent(jsonBody.getBytes(StandardCharsets.UTF_8));
+
+        //when & then
+        Assertions.assertThrows(InternalAuthenticationServiceException.class,
+                () -> loginFilter.attemptAuthentication(request, response));
+
+    }
+
+    @Test
+    @DisplayName("로그인 인증에 필요한 값이 유효하지 않다면 예외를 던진다")
+    void loginRequest_value_invalid() {
+        //given
+        String jsonBodyWithEmptyEmail = "{\"email\":\"\",\"password\":\"123456789\"}";
+        request.setContent(jsonBodyWithEmptyEmail.getBytes(StandardCharsets.UTF_8));
+
+        //when & then
+        Assertions.assertThrows(IllegalArgumentException.class,
+                () -> loginFilter.attemptAuthentication(request, response));
+
+
+        //given
+        String jsonBodyWithEmptyPassword = "{\"email\":\"test@test.com\",\"password\":\"\"}";
+        request.setContent(jsonBodyWithEmptyPassword.getBytes(StandardCharsets.UTF_8));
+
+        //when & then
+        Assertions.assertThrows(IllegalArgumentException.class,
+                () -> loginFilter.attemptAuthentication(request, response));
+
     }
 }
