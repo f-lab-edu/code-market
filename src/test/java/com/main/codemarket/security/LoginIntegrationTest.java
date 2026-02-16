360258322cefa7400a2918bdb13460b273eeacdd
KimYoonMin <dbsalszz@naver.com>
2026-02-16 20:33:09 +0900

    feat(auth): 로그인 통합 테스트 작성 - BDD 기반 시나리오성 통합 테스트 작성

--- src/test/java/com/main/codemarket/security/LoginIntegrationTest.java
+++ src/test/java/com/main/codemarket/security/LoginIntegrationTest.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
@@ -38,16 +39,20 @@ public class LoginIntegrationTest {
 
     private LoginRequestDto loginRequestDto;
 
+    private BCryptPasswordEncoder passwordEncoder;
+
     @BeforeEach
     void set_up() {
         loginRequestDto = new LoginRequestDto("test@example.com", "test_password");
+        passwordEncoder = new BCryptPasswordEncoder();
     }
 
     @Test
     @DisplayName("로그인 요청을 올바른 입력값으로 요청하면 클라이언트는 새로운 토큰 값을 리턴받는다")
     void login_success() throws Exception {
         //given
-        member = Member.createMember("test@example.com", "test_user", "test_password");
+        String encryptedPassword = passwordEncoder.encode("test_password");
+        member = Member.createMember("test@example.com", "test_user", encryptedPassword);
         memberRepository.save(member);
 
         //when
