244ddb1f5dec0e6a642f98cef335884ff988a2e8
KimYoonMin <dbsalszz@naver.com>
2026-02-17 23:28:46 +0900

    feat(auth): CustomUserDetail 객체 추가
    (cherry picked from commit fd1e6f15a1096aa76d1fd5c2e2c3d9fae4d8e13b)

---
+++ src/main/java/com/main/codemarket/security/CustomUserDetail.java
@@ -0,0 +1,32 @@
+package com.main.codemarket.security;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import java.util.Collection;
+import java.util.List;
+
+public class CustomUserDetail implements UserDetails {
+    private final String email;
+    private final String password;
+
+    public CustomUserDetail(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+}
